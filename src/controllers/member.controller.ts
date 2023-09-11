3f8cbb2e0e2b50f4ae277f9109e5771cdf9990f7
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-09-11 07:28:06 +0530

    updated

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -53,30 +53,101 @@ export const addMember = async (req: Request, res: Response) => {
         }
     });
 }
+
 export const removeMember = async (req: Request, res: Response) => {
-    const { userId } = req.params;
-    const db = getDatabase();
-    if (!db) {
-        return res.status(500).json({ success: false, error: 'Database connection error' });
+    const userId = req.params;
+    try {
+
+        const db = getDatabase();
+        if (!db) {
+            return res.status(500).json({ success: false, error: 'Database connection error' });
+        }
+        const memberCollection = db.collection('member');
+        const usersCollection = db.collection('users');
+        const communitiesCollection = db.collection('communities');
+        console.log(userId.id);
+        const memberDetails = await memberCollection.findOne({ id: userId.id });
+        console.log(memberDetails);
+        if (!memberDetails) {
+            return res.status(404).json({ success: false, error: 'Member not found' });
+        }
+        
+        const result = await memberCollection.aggregate([
+            {
+                $match: { community: memberDetails.community },
+            },
+            {
+                $lookup: {
+                    from: 'communities',
+                    localField: 'community',
+                    foreignField: 'id',
+                    as: 'community',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'user',
+                    foreignField: 'id',
+                    as: 'user',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'roles',
+                    localField: 'role',
+                    foreignField: 'id',
+                    as: 'role',
+                }
+            },
+            {
+                $unwind: '$role',
+            },
+            {
+                $unwind: '$community',
+            },
+            {
+                $unwind: '$user',
+            },
+            {
+                $project: {
+                    _id: 0,
+                    communityId: '$community.id',
+                    ownerId: '$community.owner',
+                    userId: '$user.id',
+                    role: '$role.name',
+
+                },
+            },
+        ]).toArray();
+        console.log(result)
+        if (result.length === 0) {
+            return res.status(404).json({ success: false, error: 'Member not found' });
+        }
+
+        const memberData = result[0];
+        // console.log(memberData)
+        const { ownerId, userId: memberId, role: role } = memberData;
+        console.log(ownerId, memberId)
+        // Check if the requestor is the owner
+        const requestorUserId = req.user.id;
+
+        if (requestorUserId !== ownerId.id) {
+            return res.status(401).json({ success: false, error: 'NOT_ALLOWED_ACCESS' });
+        }
+
+        // Delete the member
+        const deleteResult = await memberCollection.deleteOne({ id: userId.id });
+
+        if (deleteResult.deletedCount === 0) {
+            return res.status(404).json({ success: false, error: 'Member not found' });
+        }
+
+        res.status(200).json({ success: true, });
+
     }
-    const memberCollection = db.collection('member');
-    const usersCollection = db.collection('users');
-    const communitiesCollection = db.collection('communities');
-    const checkIfMemberExists = await memberCollection.findOne({ id: userId });
-    console.log(checkIfMemberExists);
-    if (!checkIfMemberExists) {
-        return res.status(409).json({ success: false, error: 'Member doesnt exists' });
+    catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ success: false, error: 'Something went wrong' });
     }
-    const community = await communitiesCollection.findOne({ id: checkIfMemberExists.community });
-    if (!community) {
-        return res.status(404).json({ success: false, error: 'Community not found' });
-    }
-    if (checkIfMemberExists.id !== community.owner) {
-        return res.status(401).json({ success: false, error: 'NOT_ALLOWED_ACCESS' });
-    }
-    const result = await memberCollection.deleteOne({ id: userId });
-    if (!result.deletedCount) {
-        return res.status(500).json({ success: false, error: "Something went wrong" });
-    }
-    res.status(200).json({ success: true, content: { data: result.deletedCount } });
 }
